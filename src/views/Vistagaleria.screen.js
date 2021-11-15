9bc3023f82a54b7a89971f36e65f52adece19d53
Alejandro Montes de Oca <la.montesdeoca.p@gmail.com>
2021-11-15 14:18:28 -0600

    ADD:

--- src/views/Vistagaleria.screen.js
+++ src/views/Vistagaleria.screen.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import {_verDocumentales} from '../api/index.api';
 import {
 	Button,
 	Card,
@@ -8,76 +7,113 @@ import {
 	CardText,
 	CardTitle,
 	Container,
+	Modal,
+	ModalBody,
+	ModalFooter,
 } from 'reactstrap';
+import VideoContentYT from 'react-video-content-youtube';
+
 import triangulo from '../assets/imgs/caret-right (1).png';
+import { _verDocumentales } from '../api/index.api';
 
 export const Vistagaleriascreen = () => {
-	const [documentales, setDocumentales] = useState([])
+	const [documentales, setDocumentales] = useState([]);
+	const [viewPlay, setViewPlay] = useState(false);
+	const [urlVideo, setUrlVideo] = useState('');
+
 	useEffect(() => {
 		getDocumentales();
-	}, [])
+	}, []);
 
-	const getDocumentales = async () => { 
+	const getDocumentales = async () => {
 		try {
 			const response = await _verDocumentales();
-			setDocumentales(response);
+			const cleanResponse = buildDocumentales(response);
+			setDocumentales(cleanResponse);
 		} catch (error) {
-			console.log(error)
+			console.log(error);
 		}
-	}
+	};
 
+	const buildDocumentales = (array = []) =>
+		array.map(documental => {
+			let obj = { ...documental };
+			obj.view = false;
+			return obj;
+		});
 
-	console.log(documentales)
-
-
+	const toogleView = () => setViewPlay(!viewPlay);
 
 	return (
 		<>
 			<section className='fondochingon d-flex justify-content-center align-items-center'>
 				<Container className=' d-flex justify-content-center align-items-center'>
-				<div className='d-flex flex-wrap'>
-					{
-					documentales.map(({Nombre,categoria,Trama}) => {
-						return (
-							
-							<Card style={{ maxWidth: 500 , margin: 10}}
-								className='cartaGrande'
-							>
-								<CardBody>
-									<CardTitle tag='h5'>{Nombre}</CardTitle>
-									<CardSubtitle className='mb-2 text-muted' tag='h6'>
-										{categoria}
-									</CardSubtitle>
-									<CardText>
-										{Trama}
-									</CardText>
-									<Button
-										style={{
-											backgroundColor: '#525C90',
-											borderRadius: 15,
-											width: 160,
-										}}
-									>
-										<img
-											src={triangulo}
-											alt='icono'
-											style={{
-												width: 20,
-												height: 20,
-												marginRight: 10,
+					<div className='d-flex flex-wrap'>
+						{documentales.map(({ Nombre, categoria, Trama, URL }) => {
+							return (
+								<Card
+									key={URL}
+									style={{ maxWidth: 500, margin: 10 }}
+									className='cartaGrande'
+								>
+									<CardBody>
+										<CardTitle tag='h5'>{Nombre}</CardTitle>
+										<CardSubtitle
+											className='mb-2 text-muted'
+											tag='h6'
+										>
+											{categoria}
+										</CardSubtitle>
+										<CardText>{Trama}</CardText>
+										<Button
+											style={styles.button}
+											onClick={() => {
+												setUrlVideo(URL);
+												toogleView();
 											}}
-										/>
-										Reproducir
-									</Button>
-								</CardBody>
-							</Card>
-							
-						);
-					})
-						}
+										>
+											<img
+												src={triangulo}
+												alt='icono'
+												style={styles.img}
+											/>
+											Reproducir
+										</Button>
+									</CardBody>
+								</Card>
+							);
+						})}
 					</div>
 				</Container>
 			</section>
+			<Modal
+				isOpen={viewPlay}
+				toggle={toogleView}
+				style={{ width: 700, height: 700 }}
+				size='lg'
+			>
+				<ModalBody>
+					<div style={{ height: 'auto', width: 670 }}>
+						<VideoContentYT src={`${urlVideo}`} params={{ autoPlay: true }} />
+					</div>
+				</ModalBody>
+				<ModalFooter>
+					<Button onClick={toogleView}>Cerrar</Button>
+				</ModalFooter>
+			</Modal>
 		</>
 	);
+};
+
+const styles = {
+	button: {
+		backgroundColor: '#525C90',
+		borderRadius: 15,
+		width: 160,
+	},
+	img: {
+		width: 20,
+		height: 20,
+		marginRight: 10,
+	},
 };
