297d7d9b72d46f6f1c6fff19dd589fd1ea3007e0
Axel Mendez <71779664+axelme28@users.noreply.github.com>
2021-11-21 18:12:11 -0600

    add postsSceen

--- src/views/Posts.Screen.js
+++ src/views/Posts.Screen.js
@@ -1,7 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Swal from 'sweetalert2';
+import { _getPosts, _sendPublication } from '../api/index.api';
 import { Sidebar } from '../components/Sidebar';
+import useForm from '../hooks/useForm';
+//post [{titulo,mensaje},]
+import icon from '../assets/icons/paper-plane (1).png';
 
+const data = {
+	id_usuario: 'rosa@rosa.com',
+};
+
+const initialState = {
+	titulo_publicacion: '',
+	texto_publicacion: '',
+	id_equipo: 1,
+	date: '2001-02-02',
+};
 export const PostsScreen = () => {
+	const [Post, setpost] = useState([]);
+	const [values, , handleInputChange, reset] = useForm(initialState);
+
+	useEffect(() => {
+		getPost();
+	}, []);
+
+	const getPost = async () => {
+		try {
+			const response = await _getPosts(data);
+			console.log(response);
+			const response2 = objArray(response);
+			setpost(response2);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleSendPublicacion = async e => {
+		e.preventDefault();
+		try {
+			const response = await _sendPublication(values);
+			// console.log(reponse);
+			if (response.ok === true) {
+				Swal.fire(response.message);
+				setTimeout(() => {
+					window.location.reload();
+				}, 2000);
+			}
+		} catch (error) {
+			console.log(error);
+			Swal.fire({
+				icon: 'error',
+				title: 'Hubo un error',
+			});
+		}
+	};
+
+	const objArray = (obj = {}) => {
+		let arr = [];
+
+		for (let key in obj) {
+			arr.push(obj[key]);
+		}
+
+		return arr;
+	};
+
+	console.log(Post);
+
 	return (
 		<>
 			<div className='app'>
@@ -10,60 +75,84 @@ export const PostsScreen = () => {
 				<main className='appMain container'>
 					<div
 						className='d-flex justify-content-center'
-						style={{ backgroundColor: '#515D8A', color: 'white' }}
+						style={{ backgroundColor: '#515D8A', color: 'white', height: 38 }}
 					>
 						<h2>Publicaciones</h2>
 					</div>
 					<div className=''>
-						<div style={{ height: '26rem' }} className='posts'>
-							<div
-								style={{ marginTop: '4rem', marginRight: '2rem' }}
-								className='message'
-							>
-								<div
-									class='card border-primary mb-3'
-									style={{ maxWidth: '18rem' }}
-								>
-									<div class='card-body'>
-										<h5 class='card-title'>Primary card title</h5>
-										<p class='card-text'>
-											Some quick example text to build on the card
-											title and make up the bulk of the card's
-											content.
-										</p>
+						<div
+							style={{
+								height: '26rem',
+								backgroundColor: '#D9E1FF',
+								borderRadius: '10',
+							}}
+							className='posts m-3 py-4 shadow  '
+						>
+							{Post.length > 0 &&
+								Post.map(({ titulo, texto }) => (
+									<div
+										style={{
+											marginTop: '0rem',
+											marginRight: '2rem',
+											marginLeft: '2rem',
+											width: '20rem',
+											display: 'block',
+										}}
+										className='message'
+									>
+										<div
+											className='card border-primary mb-3'
+											style={{ maxWidth: '18rem' }}
+										>
+											<div className='card-body'>
+												<h5 className='card-title'>{titulo}</h5>
+												<p className='card-text'>{texto}</p>
+											</div>
+										</div>
 									</div>
-								</div>
-							</div>
+								))}
 						</div>
 						<div
 							className='d-flex align-items-center justify-content-center'
+							style={{ marginTop: '5rem' }}
 							data-spy='scroll'
 						>
-							<form class='form d-fex'>
-								<div class='mb-3'>
+							<form className='form d-fex'>
+								<div className='mb-3'>
 									<input
 										type='text'
-										class='form-control'
+										className='form-control'
 										id='formGroupExampleInput'
+										name='titulo_publicacion'
 										placeholder='Titulo de la publicacion'
 										style={{ width: '35rem' }}
+										onChange={handleInputChange}
 									/>
 								</div>
-								<div class='mb-3'>
+								<div className='mb-3'>
 									<textarea
-										class='form-control'
+										className='form-control'
 										placeholder='Mesage de la publicacion'
 										id='floatingTextarea'
+										name='texto_publicacion'
 										style={{ width: '35rem' }}
+										onChange={handleInputChange}
 									/>
 								</div>
-								<button
-									type='submit'
-									className='btn btn-primary mb-2 ml-2'
-								>
-									Publicar
-								</button>
 							</form>
+
+							<button
+								type='submit'
+								className='btn mb-2 ml-4'
+								style={{ border: '2px solid #515D8A' }}
+								onClick={handleSendPublicacion}
+							>
+								<img
+									src={icon}
+									style={{ width: 25, marginRight: '1rem' }}
+								/>
+								Enviar
+							</button>
 						</div>
 					</div>
 				</main>
